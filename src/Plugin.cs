04a78a57d43bf320ca39f63828bbaabc3ce70505
kadu1669 <87271195+kadu1669@users.noreply.github.com>
2026-09-12 06:29:05 -0400

    Add files via upload

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -26,7 +26,7 @@ namespace NoVikingLeftBehind
     {
         public const string PluginGuid = "Nosferatu.NoVikingLeftBehind";
         public const string PluginName = "NoVikingLeftBehind";
-        public const string PluginVersion = "0.10.3";
+        public const string PluginVersion = "0.10.4";
 
         internal static ManualLogSource Log;
         internal static ConfigSync ConfigSync;
